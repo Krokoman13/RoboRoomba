54550d2f0d423c9aebb3703110e30e62640285fa
Krokoman13 <bramtehaken@gmail.com>
2018-06-24 21:18:33 +0200

    Update VacFunc.h

--- VacFunc.h
+++ VacFunc.h
@@ -1,11 +1,8 @@
-#include "VacClass.h"
+#include "mbed.h"
 
-void Move(Motor motorL, Motor motorR);		//Actually move the motors based on the speed and direction given
-void Move(Motor motorL, Motor motorR, int sec);
-void Stop(Motor motor1, Motor motor2);		//Stop the motor
-bool Detect(DistSensor dist, DistSensor dist2, DistSensor dist3);
-bool DetectO(DistSensor dist);
-bool DetectD(DistSensor dist);
-int Battery();
-bool Charge();
-void driveto(Cords, Motor, Motor);
+bool start();
+void stop();
+void initiate();
+void straight();
+void move(bool, bool, double);
+double frontdist();
