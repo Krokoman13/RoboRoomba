91669c59e34b33ed68a514ebf39983192c37715e
Krokoman13 <bramtehaken@gmail.com>
2018-06-24 21:42:45 +0200

    Update Race skelet.cpp

--- Race skelet.cpp
+++ Race skelet.cpp
@@ -5,62 +5,49 @@ int main()
 {
 	enum State { start, wait, move, obstacle, stop};
 	State states = start;
-
-	Motor motorR(true, 1.0);
-	Motor motorL(true, 1.0);
-
-	DistSensor front;
-	DistSensor under;
-	DistSensor right;
-
-	DigitalIn button();
-
-	bool second -
-	int Angle = 1; 
+	int Angle = 0; 
 	bool loop = true;
 
 	while (loop == true) {		//The program stops when loop is set to false
 		switch (states) {
 		case start:
+			initiate();		//Initiatates the period for pwm signal towards the motors
 			states = wait;
+			while (start() == true) {wait(0.1);};
 			break;
 
 		case wait:
-			if (under.DistMM() > 20 || front.DistMM() < 20) {		//If there is a object or a drop
-				Stop(motorL, motorR);		//Stop both motors
+			if (FrontDist.distance() > 15 || UnderDist.distance() < 15) {		//If there is a object or a drop
+				Stop();		//Stop both motors
 				states = obstacle;		//Go to the obstacle state
 			}
 
-			else if (button == true) states = stop;
+			else if (start() == false) states = stop;
 
-			else Move(motorL, motorR);	//if there is no obstacle just drive on
+			else straight();	//if there is no obstacle just drive on
 			break;
 
 		case obstacle:
-			if (under.DistMM() > 20 || front.DistMM() < 20) {		//If there still an obstacle
-				motorL.set_dir(false);		//Set the motors to turn left
-				motorR.set_dir(true);
-				Move(motorL, motorR);		//Make a slight turn left
-				Angle++;
+			if if (FrontDist.distance() > 15 || UnderDist.distance() < 15) {		//If there still an obstacle
+			move(false, true, 0.1)		//Make a slight turn left
+			Angle++;
 			}
 
 			else if (right.DistMM() > 30) {		//If there is no obstacle on the right
 				motorL.set_dir(true);		//set the motors to turn right
 				motorR.set_dir(false);
-				Move(motorL, motorR, Angle);		//Make a turn equal to the turn to the left
+				move(true, false, Angle * 0.1);		//Make a right turn equal to the turn to the left
 				Angle = 0;
 				states = wait;		//go back to wait
 			}
 
 			else {		//If there is no object in front of the robot but there is one on the right
-				motorL.set_dir(true);		//set the motors to drive staight
-				motorR.set_dir(true);
-				Move(motorL, motorR);		//Drive straight
+				straight();		//Drive straight
 			}
 			break;
 
 		case stop:
-			Stop(motorL, motorR);		//Stop robot
+			Stop();		//Stop robot
 			loop = false;		//Stop the program
 			break;
 		}
